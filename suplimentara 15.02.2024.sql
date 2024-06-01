6ffc500eefd45168a2b3aa017087cfabd75a006d
nataliaTanasi <nataliatanasi4@gmail.com>
2024-06-01 13:54:23 +0300

    Add files via upload

---
+++ suplimentara 15.02.2024.sql
@@ -0,0 +1,13 @@
+SELECT ad_date, campaign_id,
+sum(spend) as total_spend, 
+sum(impressions) as total_impressions,
+sum(clicks) as total_clicks,   
+sum(value) as total_value,
+sum(spend)/sum(clicks) as CPC,
+sum(spend)*1000 as CPM,
+((sum(value)::numeric-sum(spend))/sum(spend))*100 as Romi,
+sum(clicks) ::numeric/sum(impressions)
+from facebook_ads_basic_daily 
+where clicks >0 and impressions >0 and spend >0
+group by ad_date, campaign_id 
+order by Romi desc  limit 1;
